501825e8c458b7a8b0c452e973a8737e7d6315e9
Tiki Horea <Tiki2134@gmail.com>
2022-09-28 10:12:21 +0300

    went back in the course

--- Zero-to-Mastery/Complete Python Dev/data types.py
+++ Zero-to-Mastery/Complete Python Dev/data types.py
@@ -19,32 +19,4 @@
 #DATA TYPES
 
 #int and float
-# print(type(2 + 4))
-# print(type(2 - 4))
-# print(type(2 * 4))
-# print(type(2 / 4))
-
-# print(2**4)
-# print(5//4)
-# print(6%4)
-
-#math functions
-# print(round(3.6))
-# print(abs(-29.3))
-
-#operator precedence
-# some operators (maths signs) have precedence over others, meaning they get executed first.
-
-# () brackets
-# ** raising to-the-power
-# * / multiply, divide
-# + - add, subtract
-
-
-#VARIABLES
-#constants
-PI = 3.14
-half_a_pie = PI/2
-double_a_pie=PI*2
-
-print (half_a_pie)
+print(type(2/4))
