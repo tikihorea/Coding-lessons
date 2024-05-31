c6b3054a8ea17b86426a4562e6878efe3ffb5115
Tiki Horea <Tiki2134@gmail.com>
2024-05-31 14:40:59 +0300

    started from scratch

--- Zero-to-Mastery/Web Dev Junior to Senior/009 Javascript/Arrays/script.js
+++
@@ -1,2 +0,0 @@
-var list = ['tiger', 'cat', 'moose', 'platypus'];
-console.log(list[3]);
--- Zero-to-Mastery/Web Dev Junior to Senior/009 Javascript/Functions/script.js
+++
@@ -1,8 +0,0 @@
-function multiply(a, b) {
-  if (a > 10 || b > 10) {
-    return "that's too hard!";
-  } else {
-    return a * b;
-  }
-}
-console.log(multiply(5, 10));
