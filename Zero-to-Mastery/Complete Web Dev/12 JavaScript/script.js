ec4d8754b177d97dbb7e7796a12975dec0643881
Tiki Horea <Tiki2134@gmail.com>
2024-07-26 12:14:36 +0300

    next

---
+++ Zero-to-Mastery/Complete Web Dev/12 JavaScript/script.js
@@ -0,0 +1,23 @@
+var database = [
+  {
+    user: {
+      username: "John",
+      password: "password",
+    },
+  },
+];
+
+var newsfeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning!",
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is sooooo cool!",
+  },
+  {
+    username: "Mitch",
+    timeline: "Javascript is preeetyy cool!",
+  },
+];
