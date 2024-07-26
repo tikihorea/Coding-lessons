1bacd9e36001da8f63cdda5eae9cd0361edfe776
Tiki Horea <Tiki2134@gmail.com>
2024-07-26 13:25:08 +0300

    next

--- Zero-to-Mastery/Complete Web Dev/12 JavaScript/script.js
+++ Zero-to-Mastery/Complete Web Dev/12 JavaScript/script.js
@@ -1,23 +1,34 @@
 var database = [
   {
-    user: {
-      username: "John",
-      password: "password",
-    },
+    username: "1",
+    password: "1",
   },
 ];
 
-var newsfeed = [
+var newsFeed = [
   {
     username: "Bobby",
     timeline: "So tired from all that learning!",
   },
   {
     username: "Sally",
-    timeline: "Javascript is sooooo cool!",
+    timeline: "Javascript is so cool!",
   },
   {
     username: "Mitch",
-    timeline: "Javascript is preeetyy cool!",
+    timeline: "Javascript is pretty cool!",
   },
 ];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+  if (user === database[0].username && pass === database[0].password) {
+    console.log(newsFeed);
+  } else {
+    alert("wrong username and password");
+  }
+}
+
+signIn(userNamePrompt, passwordPrompt);
