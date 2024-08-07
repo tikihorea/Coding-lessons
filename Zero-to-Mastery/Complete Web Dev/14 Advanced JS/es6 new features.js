2eb38067ffe2878a52a73373e9edaa0c4b49924f
Tiki Horea <Tiki2134@gmail.com>
2024-08-07 22:55:30 +0300

    es6 new features

---
+++ Zero-to-Mastery/Complete Web Dev/14 Advanced JS/es6 new features.js
@@ -0,0 +1,56 @@
+// let + const
+
+// const player = "bobby";
+// let experience = 100;
+// let level = false;
+
+// if (experience > 90) {
+//   let level = true;
+// }
+
+// const obj = {
+//   player: "bobby",
+//   experience: 100,
+//   level: false,
+// };
+
+// destructuring
+
+// const obj = {
+//   player: "bobby",
+//   experience: 100,
+//   level: false,
+// };
+
+// slower, nastier way of getting properties
+// const player = obj.player;
+// const experience = obj.experience;
+// let level = obj.level;
+
+// faster, prettier way of getting properties
+// const { player, experience } = obj;
+// const name = "Joe";
+// const age = 34;
+// const pet = "horse";
+
+// const greeting = `Hello ${name} you seem to be ${
+//   age - 10
+// }. What do you think of your ${pet}?`;
+
+// console.log(greeting);
+
+// default argument
+
+//symbols
+
+// let sym1 = Symbol();
+// let sym2 = Symbol("foo");
+// let sym3 = Symbol("foo");
+
+//arrow functions
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const add2 = (a, b) => a + b;
