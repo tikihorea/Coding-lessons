5aa44b75da1da83a50ab687663c96ff868183a36
Tiki Horea <Tiki2134@gmail.com>
2024-08-27 13:29:44 +0300

    next

--- Zero-to-Mastery/Complete Web Dev/14 Advanced JS/exercises/exercise6-SOLUTIONS.js
+++ Zero-to-Mastery/Complete Web Dev/14 Advanced JS/exercises/exercise6-SOLUTIONS.js
@@ -11,8 +11,8 @@ const object4 = { a: 5}; // 5
 object1.a = 4;
 
 
-//#3 create two classes: an Animal class and a Mamal class. 
-// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+//#3 create two classes: an Animal class and a Mammal class.
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color.
 class Animal {
 	constructor(name, type, color) {
 		this.name = name;
@@ -21,13 +21,13 @@ class Animal {
 	}
 }
 
-class Mamal extends Animal {
+class Mammal extends Animal {
 	constructor(name, type, color) {
 		super(name, type, color)
 	}
 	sound() {
-		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
+		console.log(`Moo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
 	}
 }
 
-const cow = new Mamal('Shelly', 'cow', 'brown');
+const cow = new Mammal('Shelly', 'cow', 'brown');
